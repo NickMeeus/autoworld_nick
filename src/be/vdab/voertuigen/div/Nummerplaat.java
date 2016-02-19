622c5e7d88b7071752fa530c182fc8d24090c62a
Mezeke <Mezeke@192.168.1.4>
2016-02-19 15:59:24 +0100

    Nummerplaat test 100%

--- src/be/vdab/voertuigen/div/Nummerplaat.java
+++ src/be/vdab/voertuigen/div/Nummerplaat.java
@@ -1,19 +1,42 @@
-/*
-* @Autor Frank
-* Dit is een oefening.
- */
-
 package be.vdab.voertuigen.div;
 
-/**
- *
- * @author frank.roelants
- */
-public class Nummerplaat {
-    private String plaat;
+import java.io.Serializable;
 
-    public Nummerplaat(String plaat) {
+public class Nummerplaat implements Serializable, Comparable {
+    private static final long serialVersionUID = 1L;
+    
+    private final String plaat;
+
+    protected Nummerplaat(String plaat) { 
         this.plaat = plaat;
     }
     
+    public String getPlaat() { 
+        return plaat; 
+    }
+    
+    @Override
+    public String toString() {
+        return plaat;
+    }
+    
+    @Override
+    public int hashCode() {
+        return plaat.hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Nummerplaat)) {
+            return false;
+        }
+       Nummerplaat n = (Nummerplaat) o;
+       return plaat.equals(n.getPlaat());
+    }
+    
+    @Override
+    public int compareTo(Object o) {
+        Nummerplaat n = (Nummerplaat) o;
+        return plaat.compareTo(n.getPlaat());
+    }
 }
